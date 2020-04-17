ea9747f727ba0a7ead9bc41894b7a65167f143ea
Anna <anna11199810@hotmail.co.uk>
2020-04-18 00:31:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class FlashCardApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [] };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      
+      <div class="container col-8">
+        <br/>
+        <h3 class="text-center">Flash Cards</h3>
+        <br/>
+        <FlashCardList items={this.state.items} />
+        <form onSubmit={this.handleSubmit} class="form-group">
+          <label htmlFor="new-flashcard-front">
+            Front of Flash Card
+          </label>
+          <input class="form-control"
+            id="new-flashcard-front"
+          />
+          <label htmlFor="new-flashcard-back">
+            Back of Flash Card
+          </label>
+          <input class="form-control"
+            id="new-flashcard-back"
+          />
+          <br/>
+          <button class="btn btn-success">
+            Add Flash Card
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    console.log(e)
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (document.getElementById("new-flashcard-front").value.length === 0 && document.getElementById("new-flashcard-back").value.length === 0) {
+      return;
+    }
+    const newItem = {
+      front: document.getElementById("new-flashcard-front").value,
+      back: document.getElementById("new-flashcard-back").value,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+    }));
+  }
 }
 
-export default App;
+class FlashCardList extends React.Component {
+  render() {
+    return (
+    <div class="container col-6 rounded form-control" style={{"paddingTop": "10%", "paddingBottom": "10%", "paddingLeft": "5%", "paddingRight": "5%"}}>
+        {this.props.items.map(item => (
+          <p key={item.id}>{item.front}</p>
+        ))}
+    </div>
+    );
+  }
+}
+
+export default FlashCardApp;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import FlashCardApp from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <FlashCardApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
