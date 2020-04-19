55d2aae5c67afd48dabeca45b5dfd0dec464c8bb
Anna <anna11199810@hotmail.co.uk>
2020-04-19 03:16:49 +0100

    Impliment importing flash cards from a yaml file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-
-
+import * as yaml from 'js-yaml';
 
 class FlashCardApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { items: [], showBack: false, currentItem: 0, 
+    this.state = { items: [], showBack: false, currentItem: 0, text: null,
       style: {"paddingTop": "15%", "paddingBottom": "15%", "paddingLeft": "15%", "paddingRight": "15%"} };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.flip = this.flip.bind(this);
     this.getNew = this.getNew.bind(this);
+    this.handleFileSelect = this.handleFileSelect.bind(this);
+    this.handleFileLoad = this.handleFileLoad.bind(this);
   }
 
   render() {
@@ -27,21 +28,82 @@ class FlashCardApp extends React.Component {
           </button>
           &nbsp; &nbsp;
           <button class="btn btn-info" onClick={this.getNew}>
-            New Flash Card
+            Another Flash Card
           </button>
         </div>
         <br/>
-        <h3 class="text-center">Add New Flash Cards</h3>
+        <h4 class="text-center">Add New Flashcards from Yaml</h4>
+        <div class="text-center">
+            <input type="file" name="file"  onChange={this.handleFileSelect}/>
+            &nbsp; &nbsp;
+            <button class="btn" onClick={function () {
+              alert('File format:\n ' +
+                     'SectionName:\n    Weight: 2\n    Description: blah blah\n    Key Knowledge Areas:\n        - Blah Blah\n    Examples:\n' +
+                     '        - / (root) filesystem\n    Questions:\n        Prompts:\n            - / (root) filesystem\n        Answers:\n            ' +
+                     '- / (root) filesystem\n\nNotes:\nMultiple top level keys are supported but they\'re all lumped together, only the Questions key and ' + 
+                     'it\'s Prompts/Answers keys are needed under each\n')}}>
+                Yaml File Format
+            </button>
+        </div>
+        <br/>
+        <h4 class="text-center">Add New Flashcards Manually</h4>
         <form onSubmit={this.handleSubmit} class="form-group">
           <input class="form-control" id="new-flashcard-front" placeholder="Front of Flash Card"/>
+        <br/>
           <input class="form-control" id="new-flashcard-back"placeholder="Back of Flash Card"/>
-          <br/>
+        <br/>
           <button class="btn btn-success">
             Add Flash Card
           </button>
         </form>
       </div>
     );
+  }
+  
+  handleFileSelect(evt) {
+      if (!evt.target.files[0].name.includes("yaml") || !evt.target.files[0].name.includes("yml")) {
+        alert("Please submit a valid yaml file")
+      }
+      var reader = new FileReader();
+  
+      // file reading started
+      reader.addEventListener('loadstart', function() {
+          console.log('File reading started');
+      });
+    
+      // file reading finished successfully
+      reader.addEventListener('load', e => this.handleFileLoad(e));
+      // file reading failed
+      reader.addEventListener('error', function() {
+          alert('Error : Failed to read file');
+      });
+      
+      reader.readAsText(evt.target.files[0]);
+  }
+
+  handleFileLoad(e) {
+    var item = []
+    this.setState({ text: yaml.safeLoad(e.target.result) });
+    for (var key of Object.keys(this.state.text)) {
+      console.log("key" + key)
+      for (var key2 of Object.keys(this.state.text[key])) {
+        console.log("key2" + key2)
+        if (!key2.match('Weight') && !key2.match('Description') && !key2.match('KeyKnowledgeAreas') && !key2.match('Examples') && !key2.match('Key Knowledge Areas')) {
+          console.log(this.state.text[key][key2])
+          for (var i=0; i<this.state.text[key][key2]['Prompts'].length; i++) {
+            item.push({
+              front: this.state.text[key][key2]['Prompts'][i],
+              back: this.state.text[key][key2]['Answers'][i],
+              id: Date.now()
+            });
+          }
+        }
+      }
+    }
+    console.log(item)
+    this.setState(state => ({
+      items: state.items.concat(item),
+    }));
   }
 
   getNew() {
@@ -71,12 +133,19 @@ class FlashCardApp extends React.Component {
     this.setState({ text: e.target.value });
   }
 
+  randomColour() { 
+    var letters = ['AliceBlue', 'Beige', 'Bisque', 'AntiqueWhite', 'BurlyWood', 'Cornsilk', 'Gainsboro', 'LightSteelBlue', 'Moccasin', 'Thistle']
+    var color = letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('flashCardContainer').style.background = color; 
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     if (document.getElementById("new-flashcard-front").value.length === 0 || document.getElementById("new-flashcard-back").value.length === 0) {
       alert("Please complete both fields")
       return;
     }
+    this.randomColour() 
     this.setState({ currentItem: Math.floor(Math.random()*this.state.items.length) });
     const newItem = {
       front: document.getElementById("new-flashcard-front").value,
