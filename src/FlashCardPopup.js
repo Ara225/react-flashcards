ceb2f439916ad196a10714803e1d6ca1c15a8709
Anna <anna11199810@hotmail.co.uk>
2020-04-21 13:44:14 +0100

    Add scrolling through flashcard logically

--- src/FlashCardPopup.js
+++ src/FlashCardPopup.js
@@ -12,15 +12,17 @@ class FlashCardPopup extends React.Component {
         };
         this.flip = this.flip.bind(this);
         this.getNew = this.getNew.bind(this);
+        this.getPrevious = this.getPrevious.bind(this);
+        this.getNext = this.getNext.bind(this);
     }
 
     render() {
         if (this.state.items.length > 0) {
             // Bodge to get this working so we don't always start with flash card 0
-            if (this.state.currentItemWasSet === false) {
-                var randomChoice = Math.floor(Math.random() * this.state.items.length)
-                this.setState({ currentItem: randomChoice, currentItemWasSet: true });
-            }
+            //if (this.state.currentItemWasSet === false) {
+            //    var randomChoice = Math.floor(Math.random() * this.state.items.length)
+            //    this.setState({ currentItem: randomChoice, currentItemWasSet: true });
+            //}
             if (this.state.showBack === false) {
                 return (
                     <div>
@@ -29,8 +31,16 @@ class FlashCardPopup extends React.Component {
                                 value={this.state.items[this.state.currentItem].front} style={this.state.style} disabled>
                             </textarea>
                             <br />
-                            <button class="btn btn-info" onClick={this.getNew}>
-                                 Get Random Flash Card
+                            <button class="btn btn-info" onClick={this.getPrevious}>
+                                Previous Flashcard
+                            </button>
+                            &nbsp; &nbsp;
+                            <button class="btn btn-primary" onClick={this.getNew}>
+                                Random Flashcard
+                            </button>
+                            &nbsp; &nbsp;
+                            <button class="btn btn-info" onClick={this.getNext}>
+                                Next Flashcard
                             </button>
                         </div>
                         <div onClick={this.flip} style={{"width":"99%","position":"absolute", "top":"5px", "left":"5px","height":"80%"}} >
@@ -46,8 +56,16 @@ class FlashCardPopup extends React.Component {
                                 value={this.state.items[this.state.currentItem].back} style={this.state.style} disabled>
                             </textarea>
                             <br />
-                            <button class="btn btn-info" onClick={this.getNew}>
-                                Get Random Flash Card
+                            <button class="btn btn-info" onClick={this.getPrevious}>
+                                Previous Flashcard
+                            </button>
+                            &nbsp; &nbsp;
+                            <button class="btn btn-primary" onClick={this.getNew}>
+                                Random Flashcard
+                            </button>
+                            &nbsp; &nbsp;
+                            <button class="btn btn-info" onClick={this.getNext}>
+                                Next Flashcard
                             </button>
                         </div>
                         <div onClick={this.flip} style={{"width":"99%","position":"absolute", "top":"5px", "left":"5px","height":"80%"}} >
@@ -74,6 +92,22 @@ class FlashCardPopup extends React.Component {
                 randomChoice = Math.floor(Math.random() * this.state.items.length)
             }
             this.setState({ currentItem: randomChoice });
+            this.randomColour()
+        }
+    }
+
+    getNext() {
+        if (this.state.items.length > 0 && this.state.currentItem+1 <= this.state.items.length-1) {
+            this.setState({ showBack: false });
+            this.setState({ currentItem: this.state.currentItem+1 });
+            this.randomColour()
+        }
+    }
+
+    getPrevious() {
+        if (this.state.items.length > 0 && this.state.currentItem !== 0) {
+            this.setState({ showBack: false });
+            this.setState({ currentItem: this.state.currentItem-1 });
             this.randomColour()
         }
     }
